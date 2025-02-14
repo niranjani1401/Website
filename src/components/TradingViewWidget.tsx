343e5f8d61e56c322f1acdbf26e97e594e34404f
niranjani1401 <niranjanihiremath@gmail.com>
2025-02-14 21:12:27 +0530

    trading BLACK

--- src/components/TradingViewWidget.tsx
+++ src/components/TradingViewWidget.tsx
@@ -9,25 +9,30 @@ const TradingViewWidget: React.FC<TradingViewWidgetProps> = ({ symbol }) => {
 
   useEffect(() => {
     if (containerRef.current) {
-      const script = document.createElement("script");
-      script.src =
-        "https://s3.tradingview.com/external-embedding/embed-widget-advanced-chart.js";
-      script.type = "text/javascript";
-      script.async = true;
-      script.innerHTML = JSON.stringify({
-        symbol: symbol || "NASDAQ:AAPL",
-        width: "100%",
-        height: 500,
-        theme: "light",
-        interval: "D",
-        timezone: "Etc/UTC",
-        style: "1",
-        locale: "en",
-        enable_publishing: false,
-        hide_side_toolbar: false,
-        allow_symbol_change: true,
-        container_id: "tradingview_widget",
-      });
+      containerRef.current.innerHTML = ""; // Clears any existing widget
+    }
+
+    const script = document.createElement("script");
+    script.src =
+      "https://s3.tradingview.com/external-embedding/embed-widget-advanced-chart.js";
+    script.type = "text/javascript";
+    script.async = true;
+    script.innerHTML = JSON.stringify({
+      symbol: symbol || "NASDAQ:AAPL",
+      width: 1500,
+      height: 500,
+      theme: "dark",
+      interval: "D",
+      timezone: "Etc/UTC",
+      style: "1",
+      locale: "en",
+      enable_publishing: false,
+      hide_side_toolbar: false,
+      allow_symbol_change: true,
+      container_id: "tradingview_widget",
+    });
+
+    if (containerRef.current) {
       containerRef.current.appendChild(script);
     }
   }, [symbol]);
